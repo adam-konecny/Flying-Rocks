2b04047edbc2a09743e1f3b3d00fb839c95cf263
Adam Konečný <adam.konecny94@gmail.com>
2024-11-23 13:49:44 +0100

    Created project.

---
+++ Flying Rocks/FlyingRocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flying_RocksApp.swift
+//  Flying Rocks
+//
+//  Created by Adam Konečný on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlyingRocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
